c500625d95ca44024a5b322b5860daf54e0bee3e
grupo8mintic <grupo8mintic@gmail.com>
2022-10-12 08:54:55 -0500

    Subiendo APP para despliegue

---
+++ app/inbox.py
@@ -0,0 +1,77 @@
+from traceback import print_list
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, current_app, send_file
+)
+
+from app.auth import login_required
+from app.db import get_db
+
+bp = Blueprint('inbox', __name__, url_prefix='/inbox')
+
+@bp.route("/getDB")
+@login_required
+def getDB():
+    return send_file(current_app.config['DATABASE'], as_attachment=True)
+
+
+@bp.route('/show')
+@login_required
+def show():
+    to_id = g.user['id']
+    db = get_db()
+    messages = db.execute(
+        'SELECT m.id, subject, body, created, from_id, username'
+        ' FROM message m JOIN user u ON m.from_id = u.id'
+        ' WHERE to_id = ?'
+        ' ORDER BY created DESC', (to_id,)
+    ).fetchall()
+    return render_template('inbox/show.html', messages=messages)
+
+
+@bp.route('/send', methods=('GET', 'POST'))
+@login_required
+def send():
+    if request.method == 'POST':        
+        from_id = g.user['id']
+        to_username = request.form['to']
+        subject = request.form['subject']
+        body = request.form['body']
+
+        db = get_db()
+       
+        if not to_username:
+            flash('To field is required','error')
+            return render_template('inbox/send.html')
+        
+        if not subject:
+            flash('Subject field is required','error')
+            return render_template('inbox/send.html')
+        
+        if not body:
+            flash('Body field is required','error')
+            return render_template('inbox/send.html')  
+        
+        error = None    
+        userto = None 
+        
+        userto = db.execute(
+            'SELECT * FROM user WHERE username = ?', (to_username,)
+        ).fetchone()
+        
+        if userto is None:
+            error = 'Recipient does not exist'
+     
+        if error is not None:
+            flash(error, 'error')
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO message (from_id, to_id, subject, body) VALUES (?, ?, ?, ?)',
+                (from_id, userto['id'], subject, body)
+            )
+            db.commit()
+            flash('Message sent successfully', 'success')
+
+            return redirect(url_for('inbox.show'))
+
+    return render_template('inbox/send.html')
